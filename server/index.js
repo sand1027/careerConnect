2c389aee9a1088f7a824faedb1160ce5d28992c0
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-11-16 23:44:10 +0530

    added frontend url in server.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://hire-hub-od5v.vercel.app',
     credentials: true
 }
 
