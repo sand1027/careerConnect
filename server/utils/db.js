e3560772dd2b814299e7354d412e3447177eaaa3
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-11-18 22:36:09 +0530

    ready for deployment added mongo url in db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://chandanchand769:chandanchand769@cluster0.hfwz0.mongodb.net/');
         console.log('mongodb connected successfully');
     } catch (error) {
         console.log(error);
