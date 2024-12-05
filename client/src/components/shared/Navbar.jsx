a1e438cc11b59d0f670fc129ae04fed0ea443db9
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-12-05 22:21:51 +0530

    remoced console log in frontend

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
                 toast.success(res.data.message);
             }
         } catch (error) {
-            console.log(error);
+
             toast.error(error.response?.data?.message || 'An error occurred');
         }
     };
