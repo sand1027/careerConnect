cf85ae283432ad68f3a26a6df4d0a2ade1654235
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-12-10 19:56:10 +0530

    added frontend url in index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://hire-hub-chandan.vercel.app',
     credentials: true
 }
 
