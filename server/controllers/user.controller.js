483a7a58521566cd857f376bf01a12502683fb00
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-12-10 21:46:47 +0530

    made httponly removed

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -124,7 +124,7 @@ export const login = async(req, res) => {
             .status(200)
             .cookie("token", token, {
                 maxAge: 24 * 60 * 60 * 1000,
-                httpOnly: true,
+                // httpOnly: true,
                 sameSite: 'none',
                 secure: true,
             })
